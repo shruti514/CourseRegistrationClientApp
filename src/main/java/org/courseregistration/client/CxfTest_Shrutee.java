19bb39a8f81df38e93fa5f2e44be877559ec76e5
shraddhakannav <shraddhakannav@gmail.com>
2015-11-13 17:07:14 -0800

    Page Class addedd

--- src/main/java/org/courseregistration/client/CxfTest_Shrutee.java
+++ src/main/java/org/courseregistration/client/CxfTest_Shrutee.java
@@ -1,25 +1,24 @@
 package org.courseregistration.client;
 
-import org.apache.cxf.jaxrs.client.WebClient;
-import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
-import org.courseregistration.client.responses.StudentResponse;
-import org.jboss.resteasy.client.ProxyFactory;
-
-import javax.ws.rs.core.Response;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
+import org.courseregistration.client.responses.StudentResponse;
+
 public class CxfTest_Shrutee {
 
-    public static void  getStudent(){
-        List<Object> providers = new ArrayList<>();
-        providers.add( new JacksonJaxbJsonProvider() );
+	public static void getStudent() {
+		List<Object> providers = new ArrayList<>();
+		providers.add(new JacksonJaxbJsonProvider());
 
-        WebClient client = WebClient.create("http://localhost:8888/api.courseregistration/", providers);
-        client = client.accept("application/json").type("application/json").path("/students/100025");
+		WebClient client = WebClient.create(
+				"http://localhost:8888/api.courseregistration/", providers);
+		client = client.accept("application/json").type("application/json")
+				.path("/students/100025");
 
-        StudentResponse studentResponse = client.get(StudentResponse.class);
-        System.out.println("Student Response:" + studentResponse.toString());
-    }
+		StudentResponse studentResponse = client.get(StudentResponse.class);
+		System.out.println("Student Response:" + studentResponse.toString());
+	}
 }
--- src/main/java/org/courseregistration/client/resources/StudentResource.java
+++ src/main/java/org/courseregistration/client/resources/StudentResource.java
@@ -1,17 +1,17 @@
 package org.courseregistration.client.resources;
 
-import org.courseregistration.client.responses.StudentResponse;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.courseregistration.client.responses.StudentResponse;
 
 public interface StudentResource {
 
-    @GET
-    @Produces("application/json")
-    @Path("/students/{id}")
-    StudentResponse getStudent(@PathParam("id") int id);
+	@GET
+	@Produces("application/json")
+	@Path("/students/{id}")
+	StudentResponse getStudent(@PathParam("id") int id);
+
 }
