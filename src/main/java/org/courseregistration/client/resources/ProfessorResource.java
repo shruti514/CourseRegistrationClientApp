d958d0b63a731046dedc85e9d1cd9825a1708c47
Shital Kadam <kadam.s.d.2009@gmail.com>
2015-11-13 17:13:43 -0800

    Created ProfessorResource

--- src/main/java/org/courseregistration/client/resources/ProfessorResource.java
+++ src/main/java/org/courseregistration/client/resources/ProfessorResource.java
@@ -2,6 +2,19 @@ package org.courseregistration.client.resources;
 
 /**
  * Created by SHITAL on 11/13/2015.
- */
-public class ProfessorResource {
+ **/
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.courseregistration.client.responses.ProfessorResponse;
+import org.courseregistration.client.responses.StudentResponse;
+
+public interface ProfessorResource {
+    @GET
+    @Produces("application/json")
+    @Path("/professors/{id}")
+    ProfessorResponse getProfessor(@PathParam("id") int id);
 }
