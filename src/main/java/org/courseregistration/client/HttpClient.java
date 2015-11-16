b96ed2959d63ce56d70d5f3be276683cd5877978
shruti <shruti.gangras514@gmail.com>
2015-11-15 18:01:11 -0800

    removed unwanted classes

--- src/main/java/org/courseregistration/client/HttpClient.java
+++
@@ -1,9 +0,0 @@
-package org.courseregistration.client;
-
-
-import org.apache.http.impl.client.CloseableHttpClient;
-
-public class HttpClient {
-
-    private CloseableHttpClient httpClient;
-}
--- src/main/java/org/courseregistration/client/ProfessorClient.java
+++ src/main/java/org/courseregistration/client/ProfessorClient.java
@@ -1,7 +1,4 @@
 package org.courseregistration.client;
 
-/**
- * Created by SHITAL on 11/13/2015.
- */
 public class ProfessorClient {
 }
