0bd47fe59b15dd98279f73cb570f4fdadbabd442
Shital Kadam <kadam.s.d.2009@gmail.com>
2015-11-16 17:01:42 -0800

    Professor changes

--- src/main/java/org/courseregistration/client/ProfessorClient.java
+++
@@ -1,4 +0,0 @@
-package org.courseregistration.client;
-
-public class ProfessorClient {
-}
--- src/main/java/org/courseregistration/client/responses/ProfessorResponse.java
+++ src/main/java/org/courseregistration/client/responses/ProfessorResponse.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 public class ProfessorResponse {
     private Professor professor;
+    private List<ProfessorResponse> content;
     private List<Link> links;
-    private List<Professor> professors;
     private Page page;
 
     public Professor getProfessor() {
@@ -20,6 +20,10 @@ public class ProfessorResponse {
         this.professor = professor;
     }
 
+    public List<ProfessorResponse> getContent(){return content;}
+
+    public void setContent(List<ProfessorResponse> content){this.content = content;}
+
     public List<Link> getLinks() {
         return links;
     }
@@ -27,4 +31,60 @@ public class ProfessorResponse {
     public void setLinks(List<Link> links) {
         this.links = links;
     }
+
+    public Page getPage(){return page;}
+    public void setPage(Page page){this.page = page;}
+    public void deleteProfessor(Professor professor) {
+
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder toReturn = new StringBuilder("ProfessorResponse{");
+        if (professor != null){
+            toReturn.append("Student : {");
+            toReturn.append(professor.toString());
+        }
+        if (content != null ){
+            toReturn.append("content : [");
+            for(ProfessorResponse response : content){
+                toReturn.append("{");
+                toReturn.append(response.toString());
+                toReturn.append("}");
+            }
+            toReturn.append("]");
+        }
+        if (links != null){
+            toReturn.append("links : [");
+            for(Link link : links){
+                toReturn.append(link.toString());
+            }
+            toReturn.append("]");
+        }
+        if(page != null){
+            toReturn.append(page.toString());
+        }
+
+        toReturn.append("}");
+        return toReturn.toString();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
