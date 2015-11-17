07352d3cb164da501948a9c540ff79d6d1a8953e
Shital Kadam <kadam.s.d.2009@gmail.com>
2015-11-17 11:46:29 -0800

    Professor changes

--- src/main/java/org/courseregistration/client/client/ProfessorClient.java
+++ src/main/java/org/courseregistration/client/client/ProfessorClient.java
@@ -6,25 +6,63 @@ import org.jboss.resteasy.client.ProxyFactory;
 
 import javax.ws.rs.core.Response;
 
-
-/**
- * Created by SHITAL on 11/13/2015.
- */
+/**Created by SHITAL on 11/13/2015.*/
 
 public class ProfessorClient {
-    //  Get information about professor
+
+    // 1.See professor profile
     public static  void getProfessor(){
        //  ResteasyClient resteasyClient = new ResteasyClientBuilder().build();
-        ProfessorResource professorResource = ProxyFactory.create(ProfessorResource.class,
-                "http://localhost:8888/api.courseregistration/");
+        ProfessorResource professorResource = ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration/");
         Response professor  = professorResource.getProfessor(100016);
         System.out.println(professor.toString());
     }
 
+    //2. update professor profile
+    public  static void updateProfessor(){
+        ProfessorResource professorResource = ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
+        ProfessorResponse professor = professorResource.updateProfessor(100016);
+        System.out.println(professor.toString());
+    }
+
+    // 3. Delete professor.
+    public static void deleteProfessor(){
+        ProfessorResource professorResource = ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
+        ProfessorResponse professor = professorResource.deleteProfessor(100026);
+        System.out.println(professor.toString());
+    }
+
+    //  4. Add new course
+    public static void addNewCourse(){
+        ProfessorResource professorResource=  ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
+        ProfessorResponse professor = professorResource.addNewCourse(100016,100004);
+    }
+
+    //  5. List of all sections
+    public static void getAllSections() {
+        ProfessorResource professorResource=  ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
+        ProfessorResponse professor = professorResource.getAllSections();
+    }
+
+    //  6. Search for a student
+    public static void getStudentDetails() {
+        ProfessorResource professorResource=  ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
+        ProfessorResponse professor = professorResource.getStudentDetails(100025);
+        System.out.println(professor.toString());
+    }
+
+    //  7. Search for a Course
+    public static void getCourseDetails() {
+        ProfessorResource professorResource=  ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
+        ProfessorResponse professor = professorResource.getCourseDetails();
+        System.out.println(professor.toString());
+    }
+
+
     //  Get information about all professors
     public static void getAllProfessors(){
-        ProfessorResource professorResource = ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration/");
-        ProfessorResponse professor = professorResource.getAllProfessors();
+        ProfessorResource professorResource=  ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
+       ProfessorResponse professor = professorResource.getAllProfessors();
         System.out.println(professor.toString());
     }
 
@@ -41,22 +79,9 @@ public class ProfessorClient {
         ProfessorResponse professor = professorResource.setMultipleProfessors();
     }
 
-    //  delete single professor
-    public static void deleteProfessor(){
-        ProfessorResource professorResource = ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
-        ProfessorResponse professor = professorResource.deleteProfessor(100026);
-        System.out.println(professor.toString());
-    }
-
     //  delete multiple professors
     public  static void deleteMultipleProfessors(){
         ProfessorResource professorResource = ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
         ProfessorResponse professor = professorResource.deleteMultipleProfessors();
-    }
-
-    //  update professor details
-    public  static void updateProfessor(){
-        ProfessorResource professorResource = ProxyFactory.create(ProfessorResource.class,"http://localhost:8888/api.courseregistration");
-        ProfessorResponse professor = professorResource.updateProfessor(100016);
     }
 }
--- src/main/java/org/courseregistration/client/resources/ProfessorResource.java
+++ src/main/java/org/courseregistration/client/resources/ProfessorResource.java
@@ -1,8 +1,6 @@
 package org.courseregistration.client.resources;
 
-/**
- * Created by SHITAL on 11/13/2015.
- **/
+/** Created by SHITAL on 11/13/2015.**/
 
 import org.courseregistration.client.responses.ProfessorResponse;
 
@@ -10,17 +8,51 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 public interface ProfessorResource {
-    //  get a single professor
+    //  1. See logged in professor profile
     @GET
     @Produces("application/json")
     @Path("/professors/{id}")
     Response getProfessor(@PathParam("id") int id);
 
-    //  get all professors
+    //  2. Update logged in professor details
+    @PUT
+    @Produces("text/plain")
+    @Consumes("application/json")
+    @Path("professor/{id}")
+    ProfessorResponse updateProfessor(@PathParam("id") int id);
+
+    //  3. Delete logged in professor profile
+    @DELETE
+    @Produces("application/json")
+    @Path("professor/{id}")
+    ProfessorResponse deleteProfessor(@PathParam("id") int id);
+
+    //   4. Add new course
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("{id}/courses/{course_id}")
+    ProfessorResponse addNewCourse(@PathParam("id") int id, @PathParam("course_id") int course_id);
+
+    //  5. List of all sections
     @GET
     @Produces("application/json")
-    @Path("/professors")
-    ProfessorResponse getAllProfessors();
+    @Consumes("application/json")
+    @Path("/sections")
+    ProfessorResponse getAllSections();
+
+    //  6. Search for a student
+    @GET
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("/students/{id}")
+    ProfessorResponse getStudentDetails(@PathParam("id") int id);
+
+    //  7. Search for a Course
+    @GET
+    @Produces("application/json")
+    @Path("/courses/{id}")
+    ProfessorResponse getCourseDetails();
 
     //  create single professor
     @POST
@@ -36,24 +68,18 @@ public interface ProfessorResource {
     @Path("/professors")
     ProfessorResponse setMultipleProfessors();
 
-    //  delete a single student
-    @DELETE
-    @Produces("application/json")
-    @Path("professor/{id}")
-    ProfessorResponse deleteProfessor(@PathParam("id") int id);
-
     //  delete multiple professors
     @DELETE
     @Produces("application/json")
     @Path("professor/{ids}")
     ProfessorResponse deleteMultipleProfessors();
 
-    //  update professors details
-    @PUT
-    @Produces("text/plain")
-    @Consumes("application/json")
-    @Path("professor/{id}")
-    ProfessorResponse updateProfessor(@PathParam("id") int id);
+    // Display all professors list
+    @GET
+    @Produces("application/json")
+    @Path("/professors")
+    ProfessorResponse getAllProfessors();
+
 
 }
 
